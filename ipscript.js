e81924502785372054b3dbf0d1c4497b7ca30e16
agambhamra <agam.bhamra@gmail.com>
2021-08-21 23:20:08 +0530

    Create ipscript.js

---
+++ ipscript.js
@@ -0,0 +1,37 @@
+<script type="text/javascript">
+$(document).ready(function(){
+  alert(12);
+	const urlParams = new URLSearchParams(window.location.search);
+	const uid = urlParams.get('uid');
+	const key = urlParams.get('key');
+	const url = urlParams.get('url');
+
+	if(uid === '' || uid === null || key === '' || key === null || url === '' || url === null) 
+		console.log('Missing required inputs');
+	else
+		$.ajax(
+	  	{
+	  		url: "https://api.ipify.org/?format=json", success: function(result)
+			{
+				const ipAdress = result.ip; 
+				$.ajax(
+				{
+					type: 'POST',
+					url: url, 
+					headers: {
+						"content-type":"application/json",
+						"authorization": key
+					},
+					data: JSON.stringify(
+						{
+							"ip": ipAdress,
+							"uid": uid
+						}
+					)
+				}
+				);
+	  		}
+	  	}
+  	);
+});
+</script>
